c5be4192d5a12b16aa7c20c1ebd5ae4224e98ffb
fredros <frer@stud.ntnu.no>
2018-04-15 16:59:12 +0200

    Småendringer

--- oblig3/chromethreads.ps1
+++ oblig3/chromethreads.ps1
@@ -1,1 +1,3 @@
+
+#Kanskje ikke på den måten som var ment, men teknisk sett 1 linje? ;D
 $procs = (Get-Process chrome).id; foreach ($proc in $procs) { $antthread = ((Get-Process | Where-Object {$_.id -eq $proc}).Threads).Count; write-output "chrome $proc $antthread" }
--- oblig3/procmi.ps1
+++ oblig3/procmi.ps1
@@ -1,5 +1,5 @@
 foreach ($id in $args) { 
-	$fil = "$id-"+(Get-Date -uformat "%Y%m%d")+".meminfo"
+	$fil = "$id-"+(Get-Date -uformat "%Y%m%d")+"-"+(Get-Date -UFormat "%H%M%S")+".meminfo"
 	$bruk = (Get-Process | Where-Object {$_.id -eq $id}).VirtualMemorySize/1Mb
     $ws = (Get-Process | Where-Object {$_.id -eq $id}).WorkingSet/1Kb
     write-output "     -----     Informasjon om minnebruk for $id     ------" > $fil
